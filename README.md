7475641b600c0321f61e254bc943b21c8404765b
edisonxiang <xiang.edison@gmail.com>
2019-06-12 00:07:34 +0800

    init project

---
+++ README.md
@@ -0,0 +1,101 @@
+# KubeEdge WeChat Demo
+
+Firstly the users input `kubeedge` to get the music tracks which can be played,
+and then the users input `play <track>` to play the music,
+at last the expected track is pushed to the edge node
+and the track is played on the speaker connected to the edge node.
+
+<img src="workflow.png">
+
+## Prerequisites
+
+### Hardware Prerequisites
+
+* RaspBerry PI (RaspBerry PI 3 has been used for this demo).
+  The RaspBerry PI is also the edge node to which the speaker will be connected.
+
+* A speaker for playing the music.
+
+### Software Prerequisites
+
+* A running Kubernetes cluster.
+
+* KubeEdge [0.3.0](https://github.com/kubeedge/kubeedge/releases/tag/v0.3.0)
+  See [instructions](https://github.com/kubeedge/kubeedge/blob/master/docs/getting-started/usage.md#run-kubeedge) on how to setup KubeEdge.
+
+* In order to control the speaker and play the expected track, we need to manage the speaker connected to the RaspBerry PI.
+  KubeEdge allows us to manage devices using Kubernetes custom resource definitions.
+  The design proposal is [here](https://github.com/kubeedge/kubeedge/blob/master/docs/proposals/device-crd.md).
+  Apply the CRD schema yamls available [here](https://github.com/kubeedge/kubeedge/tree/master/build/crds/devices) using kubectl. 
+
+## Steps to run the demo
+
+### Get demo code
+
+Clone the demo github [repo](https://github.com/edisonxiang/kubeedge-wechat-demo)
+
+### Create the device model and device instance for the speaker
+
+With the Device CRD APIs now installed in the cluster,
+we create the device model and instance for the speaker using the yaml files.
+
+```sh
+$ cd $GOPATH/github.com/edisonxiang/kubeedge-wechat-demo/kubeedge-wechat/deployments/
+$ kubectl create -f kubeedge-speaker-model.yaml
+$ kubectl create -f kubeedge-speaker-instance.yaml
+```
+
+### Create Secret for WeChat Credentials
+
+The KubeEdge WeChat App in the demo needs to serve behind WeChat.
+For this the application needs to sign the requests with a WeChat Account.
+Create a Kubernetes [Secret](https://github.com/edisonxiang/kubeedge-wechat-demo) `wechatsecret` with the credentials as below : 
+
+```sh
+kubectl create secret generic wechatsecret --from-literal=CONSUMER_KEY=<your_consumer_key> --from-literal=CONSUMER_SECRET=<your_consumer_secret> --from-literal=ACCESS_TOKEN=<your_access_token> --from-literal=ACCESS_TOKEN_SECRET=<your_access_token_secret>
+```
+
+### Run KubeEdge WeChat App
+
+The KubeEdge WeChat App runs in a VM on cloud and serve for WeChat.
+It can be deployed using a Kubernetes deployment yaml.
+
+```sh
+$ cd $GOPATH/github.com/edisonxiang/kubeedge-wechat-demo/kubeedge-wechat/deployments/
+$ kubectl create -f kubeedge-wechat-app.yaml
+```
+
+### Build PI Player App
+
+Cross-complie the PI Player App which will run on the RaspBerry PI and play the expected track.
+
+```sh
+$ cd $GOPATH/github.com/edisonxiang/kubeedge-wechat-demo/pi-player-app/
+$ export GOARCH=arm
+$ export GOOS="linux"
+$ export GOARM=6
+$ export CGO_ENABLED=1
+$ export CC=arm-linux-gnueabi-gcc
+$ ss
+```
+
+### Run PI Player App
+
+Make sure the MQTT broker is running on the RaspBerry PI.
+Copy the PI Player App binary to the RaspBerry PI and run it.
+The App will subscibe to the `$hw/events/device/speaker-01/twin/update/document` topic 
+and when it receives the expected track on the topic, it will play it on the speaker.
+
+```sh
+$ ./pi-player-app
+```
+
+### Play music by visiting WeChat Subscription Account
+
+* Visit to the WeChat Subscription Account.
+
+* Input `kubeedge` to get the music tracks which can be played.
+
+* Input `kubeedge play <track>` to play the music,
+  the track info is pushed to the RaspBerry PI
+  and the music is played on the speaker.
---
+++ kubeedge-wechat-app/main.go
@@ -0,0 +1,211 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+	"github.com/yaotian/gowechat"
+	"github.com/yaotian/gowechat/mp/message"
+	"github.com/yaotian/gowechat/mp/user"
+	"github.com/yaotian/gowechat/wxcontext"
+
+	"github.com/edisonxiang/kubeedge-wechat-demo/kubeedge-wechat-app/utils"
+	"github.com/kubeedge/kubeedge/cloud/pkg/devicecontroller/apis/devices/v1alpha1"
+
+	"k8s.io/client-go/rest"
+)
+
+// DeviceStatus is used to patch device status
+type DeviceStatus struct {
+	Status v1alpha1.DeviceStatus `json:"status"`
+}
+
+// The device id of the speaker
+var deviceID = "speaker-01"
+
+// The default namespace in which the speaker device instance resides
+var namespace = "default"
+
+// A regular expression which evaluates to true if the tweeted text
+// contains `kubeedge play`.
+var r = regexp.MustCompile(`\w*play\w*`)
+
+// The CRD client used to patch the device instance.
+var crdClient *rest.RESTClient
+
+var appURL = "http://localhost:80"
+
+// WeChat config
+var config wxcontext.Config
+
+func main() {
+
+	// Create a client to talk to the K8S API server to patch the device CRDs
+	kubeConfig, err := utils.KubeConfig()
+	if err != nil {
+		log.Fatalf("Failed to create KubeConfig , error : %v", err)
+	}
+	crdClient, err = utils.NewCRDClient(kubeConfig)
+	if err != nil {
+		log.Fatalf("Failed to create device crd client , error : %v", err)
+	}
+
+	// Set WeChat config from the secret mounted in the container.
+	err = setWeChatConfigFromSecret()
+	if err != nil {
+		log.Fatalf("Failed to create device crd client , error : %v", err)
+	}
+
+	// Listen
+	beego.Any("/", textHandler)
+	beego.Any("/oauth", wxOAuth) //需要网页授权的页面url  /oauth?target=url
+	beego.Run(":80")
+}
+
+// readWeChatCredentialsFromSecret uses the utility functions
+// to read the credentials to authorize the application with WeChat.
+func setWeChatConfigFromSecret() error {
+	var err error
+	appID, err := utils.ReadSecretKey(utils.AppID)
+	if err != nil {
+		log.Fatalf("Error reading %s : %v", utils.AppID, err)
+		return err
+	}
+	appSecret, err := utils.ReadSecretKey(utils.AppSecret)
+	if err != nil {
+		log.Fatalf("Error reading %s : %v", utils.AppSecret, err)
+		return err
+	}
+	token, err := utils.ReadSecretKey(utils.Token)
+	if err != nil {
+		log.Fatalf("Error reading %s : %v", utils.Token, err)
+		return err
+	}
+	encodingAESKey, err := utils.ReadSecretKey(utils.EncodingAESKey)
+	if err != nil {
+		log.Fatalf("Error reading %s : %v", utils.EncodingAESKey, err)
+		return err
+	}
+
+	// Contruct config
+	config = wxcontext.Config{
+		AppID:          appID,
+		AppSecret:      appSecret,
+		Token:          token,
+		EncodingAESKey: encodingAESKey,
+	}
+
+	return nil
+}
+
+//wxOAuth 微信公众平台，网页授权
+func wxOAuth(ctx *context.Context) {
+	var wechat = gowechat.NewWechat(config)
+	mp, err := wechat.MpMgr()
+	if err != nil {
+		return
+	}
+
+	oauthHandler := mp.GetPageOAuthHandler(ctx.Request, ctx.ResponseWriter, appURL+"/oauth")
+
+	oauthHandler.SetFuncCheckOpenIDExisting(func(openID string) (existing bool, stopNow bool) {
+		//看自己的系统中是否已经存在此openID的用户
+		//如果已经存在， 调用自己的Login 方法，设置cookie等，return true
+		//如果还不存在，return false, handler会自动去取用户信息
+		return false, true
+	})
+
+	oauthHandler.SetFuncAfterGetUserInfo(func(user user.Info) bool {
+		//已获得用户信息，这里用信息做注册使用
+		//调用自己的Login方法，设置cookie等
+		return false
+	})
+
+	oauthHandler.Handle()
+}
+
+// textHandler
+func textHandler(ctx *context.Context) {
+	// 微信平台mp
+	var wechat = gowechat.NewWechat(config)
+	mp, err := wechat.MpMgr()
+	log.Printf("Get MpMgr: %v", mp)
+	if err != nil {
+		log.Fatalf("Failed to get MpMgr: %v", err)
+		return
+	}
+
+	// 传入request和responseWriter
+	msgHandler := mp.GetMsgHandler(ctx.Request, ctx.ResponseWriter)
+	log.Printf("Get msgHandler: %v", msgHandler)
+
+	// 设置接收消息的处理方法
+	msgHandler.SetHandleMessageFunc(func(msg message.MixMessage) *message.Reply {
+		log.Printf("Got text %s\n", msg.Content)
+		requestText := strings.ToLower(strings.TrimSpace(msg.Content))
+
+		replyText := ""
+		if requestText == "kubeedge" {
+			replyText += "今日音乐推荐：\n"
+			replyText += "1. 至少还有你\n"
+			replyText += "2. 剩下的盛夏\n"
+			replyText += "3. 普通朋友\n"
+			replyText += "4. 起风啦\n"
+			replyText += "5. 多余的解释\n"
+			replyText += "请回复play+数字开始播放音乐\n"
+			replyText += "例如：play 1\n"
+		} else if r.MatchString(requestText) {
+			items := r.Split(requestText, -1)
+			songTrack := strings.TrimSpace(items[1])
+			log.Printf("Music Track: %s\n", songTrack)
+			// Update device twin
+			UpdateDeviceTwinWithDesiredTrack(songTrack)
+			replyText += "正在尝试播放音乐，请稍等\n"
+		} else {
+			log.Println("Could not parse the message")
+			replyText += "不能识别您的输入\n"
+		}
+		return &message.Reply{message.MsgTypeText, message.NewText(replyText)}
+	})
+
+	// 处理消息接收以及回复
+	err = msgHandler.Handle()
+	if err != nil {
+		log.Fatalf("Failed to handle message: %v", err)
+	}
+}
+
+// UpdateDeviceTwinWithDesiredTrack patches the desired state of
+// the device twin with the track to play.
+func UpdateDeviceTwinWithDesiredTrack(track string) bool {
+	status := buildStatusWithDesiredTrack(track)
+	deviceStatus := &DeviceStatus{Status: status}
+	body, err := json.Marshal(deviceStatus)
+	if err != nil {
+		log.Fatalf("Failed to marshal device status %v", deviceStatus)
+		return false
+	}
+	result := crdClient.Patch(utils.MergePatchType).Namespace(namespace).Resource(utils.ResourceTypeDevices).Name(deviceID).Body(body).Do()
+	if result.Error() != nil {
+		log.Fatalf("Failed to patch device status %v of device %v in namespace %v \n error:%+v", deviceStatus, deviceID, namespace, result.Error())
+		return false
+	} else {
+		log.Printf("Track [ %s ] will be played on speaker %s", track, deviceID)
+	}
+	return true
+}
+
+func buildStatusWithDesiredTrack(song string) v1alpha1.DeviceStatus {
+	metadata := map[string]string{"timestamp": strconv.FormatInt(time.Now().Unix()/1e6, 10),
+		"type": "string",
+	}
+	twins := []v1alpha1.Twin{{PropertyName: "track", Desired: v1alpha1.TwinProperty{Value: song, Metadata: metadata}}}
+	devicestatus := v1alpha1.DeviceStatus{Twins: twins}
+	return devicestatus
+}
